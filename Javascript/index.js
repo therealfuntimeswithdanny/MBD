e0525314a35411a045d079c239bcc76a83b44039
funtimeswithdanny <dannymorrisey1@outlook.com>
2024-10-04 15:34:42 -0400

    Added offline pup-up

--- Javascript/index.js
+++ Javascript/index.js
@@ -100,4 +100,24 @@ function loadGoogleTranslate() {
     var element = document.body;
     element.classList.toggle("dark-mode");
  }
- 
+ window.addEventListener('offline', function() { 
+
+  document.getElementById('offline-popup').style.display = 'block'; 
+
+}); 
+
+
+
+window.addEventListener('online', function() { 
+
+  document.getElementById('offline-popup').style.display = 'none'; 
+
+}); 
+
+
+
+function closePopup() { 
+
+  document.getElementById('offline-popup').style.display = 'none'; 
+
+} 
